09a34b07f394d8033f22b767d0c749c877ec9ea1
puru1joy <puru1joy@gmail.com>
2016-04-03 18:29:45 +0530

    more styles

--- js/player.js
+++ js/player.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const BrowserWindow = require('electron').remote.BrowserWindow;
+
 $(document).ready(function () {
   let v = document.getElementsByTagName('video')[0];
+  // play or pause the media
   $('#play').click(function () {
     let $i = $(this).children('i');
     // toggle playing state
@@ -14,6 +17,11 @@ $(document).ready(function () {
     // toggle classes and playing state
     playerState.playing = !playerState.playing;
     $i.toggleClass('play pause');
+  });
+  // maximize or minimize the window
+  $('#fullscreen').click(function () {
+    var bWindow = BrowserWindow.getFocusedWindow();
+    bWindow.setFullScreen(!bWindow.isFullScreen());
   });
   // trigger all the tooltips
   $('.activating').popup();
